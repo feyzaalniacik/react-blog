93ded71af72e225a1cb82133c0c51ff2a6ce1816
feyzaalniacik <92380767+feyzaalniacik@users.noreply.github.com>
2022-05-11 13:30:41 +0300

    commit

--- api/index.js
+++ api/index.js
@@ -3,13 +3,20 @@ const app = express();
 const dotenv = require("dotenv");
 const mongose = require("mongoose");
 const { default: mongoose } = require("mongoose");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
+app.use(express.json());
+
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
-}).then(console.log("Connected to MongoDB")).catch(err => console.log(err));
+})
+.then(console.log("Connected to MongoDB"))
+.catch(err => console.log(err));
+
+app.use("/api/auth", authRoute);
 
 app.listen("5000",() => {
     console.log("Backend is running.");
---
+++ api/routes/auth.js
@@ -0,0 +1,36 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require('bcrypt');
+
+// REGİSTER
+router.post("/register", async (req, res) => {
+    try {
+        const salt =await bcrypt.genSalt(10);
+        const heshedPass = await bcrypt.hash(req.body.password, salt);
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: heshedPass,
+        });
+        const user = await newUser.save();
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// LOGIN
+router.post("/login", async (req, res) => {
+    try {
+        const user = User.findOne({username: req.body.username})
+        !user && res.status(400).json("wrong credentials!")
+        const validate = await bcrypt.compare(req.body.password, user.password)
+        !validated && res.status(400).json("wrong credentials!")
+        const {password, ...others} = user;
+        
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+module.exports = router;
