006a79477c461d1baf4b255e0735d6574e268a42
feyzaalniacik <92380767+feyzaalniacik@users.noreply.github.com>
2022-05-11 14:51:53 +0300

    commit

--- api/index.js
+++ api/index.js
@@ -4,6 +4,9 @@ const dotenv = require("dotenv");
 const mongose = require("mongoose");
 const { default: mongoose } = require("mongoose");
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/user");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer");
 
 dotenv.config();
 app.use(express.json());
@@ -16,7 +19,23 @@ mongoose.connect(process.env.MONGO_URL, {
 .then(console.log("Connected to MongoDB"))
 .catch(err => console.log(err));
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "images")
+    }, filename:(req, file, cb) => {
+        cb(null, req.body.name); //postman'a atanacak
+    },
+});
+
+const uploat = multer({storage: storage});
+app.post("/api/upload", uploat.single("file"), (req, res) => {
+    res.status(200).json("File has been uploaded");
+});
+
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
 
 app.listen("5000",() => {
     console.log("Backend is running.");
