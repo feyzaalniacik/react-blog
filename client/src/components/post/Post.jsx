3ddb1f39f0bcd6a41161ade11ffdf8d58910e98b
feyzaalniacik <92380767+feyzaalniacik@users.noreply.github.com>
2022-05-11 15:50:31 +0300

    commit

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,19 +1,26 @@
-import "./post.css"
+import "./post.css";
+import { Link } from "react-router-dom";
 
-export default function Post() {
+export default function Post({ post }) {
+  const PF = "http://localhost:5000/images/";
   return (
     <div className="post">
-        <img className="postImg" src="" alt="" />
-        <div className="postInfo">
-            <div className="postCats">
-                <span className="postCat">Music</span>
-                <span className="postCat">Life</span>
-            </div>
-            <span className="postTitle">Lorem dipisicing. Id, molestiae?</span>
-            <hr />
-            <span className="postDate">1 hour ago</span>
+      {post.photo && <img className="postImg" src={PF + post.photo} alt="" />}
+      <div className="postInfo">
+        <div className="postCats">
+          {post.categories.map((c) => (
+            <span className="postCat">{c.name}</span>
+          ))}
         </div>
-        <p className="postDesc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus possimus distinctio modi maxime pariatur, nesciunt eius fuga cum error repellat?</p>
+        <Link to={`/post/${post._id}`} className="link">
+          <span className="postTitle">{post.title}</span>
+        </Link>
+        <hr />
+        <span className="postDate">
+          {new Date(post.createdAt).toDateString()}
+        </span>
+      </div>
+      <p className="postDesc">{post.desc}</p>
     </div>
-  )
+  );
 }
