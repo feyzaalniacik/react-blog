f8fba6a32596a5645efda0892bc18f28923dcd14
feyzaalniacik <92380767+feyzaalniacik@users.noreply.github.com>
2022-05-13 12:22:59 +0300

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -2,24 +2,19 @@ import Home from "./pages/home/Home";
 import TopBar from "./components/topbar/TopBar";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
-import Settings from "./pages/settings/Setting";
+import Settings from "./pages/settings/Settings";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router,  HashRouter, Route} from "react-router-dom";
 import { useContext } from "react";
 import { Context } from "./context/Context";
 
 function App() {
-  const {user} = useContext(Context); // değiştirilince url'ye göre aşağıdaki ssayfalar açılır.
+  const {user} = useContext(Context);
   return (
-    <Router>
+    <Route>
       <TopBar />
-      <Switch>
+      < HashRouter>
           <Route exact path="/">
             <Home />
           </Route>
@@ -27,11 +22,11 @@ function App() {
           <Route path="/login">{user ? <Home/> :<Login />}</Route>
           <Route path="/write">{user ? <Write/> :<Register />}</Route>
           <Route path="/settings">{user ? <Settings/> :<Register />}</Route>
-          <Route path="/post/:postId">
+          <Router path="/post/:postId">
             <Single />
-          </Route>
-        </Switch>
-    </Router>
+          </Router>
+       </ HashRouter>
+    </Route>
   );
 }
 
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState} from "react";
 import Header from "../../components/header/Header";
 import Posts from "../../components/posts/Posts";
 import Sidebar from "../../components/sidebar/Sidebar";
